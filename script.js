c2fa2ed93a5d9cfc9cb25724573041b88ad87bb4
Anatolii Orlov <anatoly.orlov93@gmail.com>
2025-03-07 03:24:43 +0400

    complicated task done

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 'use strict'
 
-const getRandomIntNumberBot = function(min, max) {
+const getRandomIntNumberBot = function(min, max, maxAttempts) {
     min = Math.floor(min);
     max = Math.ceil(max);
     let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    let attempts = 0;
 
     function isNumber (num) {
         return!isNaN(parseFloat(num) && isFinite(num));
     };
 
     function getPromptMessage () { 
-        let startPrompt = prompt(`Угадай число от ${min} до ${max}`);
+
+        if(attempts >= maxAttempts) {
+            alert(`Вы исчерпали все попытки. Загаданное число было: ${randomNumber}`);
+
+            if (confirm("Хотите сыграть еще?")) {
+                attempts = 0;
+                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+                return getPromptMessage();
+            } else {
+                alert("Спасибо за игру!");
+                return;
+            }
+        }
+
+        let remainigAttempts = maxAttempts - attempts;
+        let startPrompt = prompt(`Угадай число от ${min} до ${max}. Осталось попыток ${remainigAttempts}`);
         
         if (startPrompt===null) {
             alert("Игра окончена");
          return;
         }
-        if (!isNumber(startPrompt)) {
+
+        attempts++
+
+        if (startPrompt.trim() === ""){
+            alert("Вы ничего не ввели! Введите число!");
+            return getPromptMessage();
+        } else if (!isNumber(startPrompt)){
             alert("Введите число!");
             return getPromptMessage();
         }
-
         startPrompt = +(startPrompt.trim());
 
         if (startPrompt > randomNumber) {
@@ -38,4 +59,4 @@ const getRandomIntNumberBot = function(min, max) {
 };
 
 
-getRandomIntNumberBot(2, 6);
+getRandomIntNumberBot(1, 100, 10);
